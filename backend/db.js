b8c252b71129fc356739baeb6ac073564962b88f
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-01-22 12:11:16 +0530

    completed step 6

--- backend/db.js
+++ backend/db.js
@@ -4,10 +4,31 @@ mongoose.connect(process.env.DATABASE_URL).then(() => {
   console.log("DB's up");
 });
 const userSchema = new mongoose.Schema({
-  username: String,
-  firstName: String,
-  lastName: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLenght: 30,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 const User = new mongoose.model("User", userSchema);
 module.exports = {
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,3 +1,69 @@
 const express = require("express");
 const router = express.Router();
+const z = require("zod");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+require("dotenv").config();
+const { User } = require("../db");
+const signupSchema = z.object({
+  username: z.string().email(),
+  firstName: z.string(),
+  lastName: z.string(),
+  password: z.string().min(6),
+});
+const signinSchema = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+});
+router.post("/signup", (req, res) => {
+  const { success } = signupSchema.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({ message: "Incorrect inputs" });
+  } else {
+    User.findOne({ username: req.body.username }).then((user) => {
+      if (user) {
+        res.status(411).json({ message: "Email already taken" });
+      } else {
+        bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+          User.create({
+            username: req.body.username,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+          }).then((user) => {
+            res.status(200).json({
+              message: "User created successfully",
+              token: jwt.sign({ userId: user._id }, process.env.JWT_SECRET),
+            });
+          });
+        });
+      }
+    });
+  }
+});
+router.post("/signin", (req, res) => {
+  const { success } = signinSchema.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({ message: "Wrong inputs" });
+  } else {
+    User.findOne({
+      username: req.body.username,
+    }).then((user) => {
+      if (user) {
+        bcrypt.compare(req.body.password, user.password, (err, result) => {
+          if (result) {
+            res.status(200).json({
+              token: jwt.sign({ userId: user._id }, process.env.JWT_SECRET),
+            });
+          } else {
+            res.status(411).json({ message: "Incorrect username/password" });
+          }
+        });
+      } else {
+        res.status(411).json({ message: "User doesn't exist" });
+      }
+    });
+  }
+});
 module.exports = router;
