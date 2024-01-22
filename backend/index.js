095b397e71e3d3c80e3334ebf2b1421f4b780425
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-01-22 11:15:38 +0530

    completed step 5

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const apiRouter = require("./routes/index");
+app.use(cors());
+app.use(bodyParser.json());
 app.use("/api/v1", apiRouter);
+app.listen(3000, () => {
+  console.log("Backend's up and running");
+});
